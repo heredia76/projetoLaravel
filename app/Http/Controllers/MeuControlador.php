361bdb531f27203f128996112c4814fc38b4462b
heredia76 <wandersontadeu2@hotmail.com>
2020-07-26 12:04:32 -0300

    teste com laravel

---
+++ app/Http/Controllers/MeuControlador.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MeuControlador extends Controller
+{
+    public function produtos() {
+        echo "<h1>Produtos</h1>";
+        echo "<ol>";
+        echo "<li>Notebook</li>";
+        echo "<li>Impressora</li>";
+        echo "<li>Mouse</li>";
+        echo "</ol>";
+    }
+  
+}
